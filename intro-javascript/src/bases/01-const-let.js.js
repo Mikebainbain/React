e7086f6ba9f60baa02d5d8bfdeac875fd1dfcdc9
Mikebainbain <mlopezb749@gmail.com>
2024-04-06 13:26:19 -0600

    Declaracion de variables y uso del let y const en js

---
+++ intro-javascript/src/bases/01-const-let.js.js
@@ -0,0 +1,16 @@
+// variables y constantes
+
+const nombre = 'Miguel';
+const apellido = 'LBarrera';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log("Mi nombre es :", nombre, apellido, "mi num:", valorDado);
+
+if(true){
+    let valorDado = 20;
+    console.log(valorDado);
+}
+
+console.log(valorDado);
